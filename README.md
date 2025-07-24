6ccaf296ee24a31d8bb9f8063be8ae0f42cf9e54
Ayush Kesharwani <152063366+kesharwaniayush@users.noreply.github.com>
2025-07-24 21:13:28 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Food-website-using-php
+# Food-Ordering-Website-Using-PHP
+I Developed Yummy - The online food ordering website using php , Visit : http://yummy.great-site.net/
+For Admin panel - http://yummy.great-site.net/admin
+Use - username : chaitanya039 and password : pansare 
+to login and see functionality by adding category and coressponding food...
---
+++ footer.php
@@ -0,0 +1,67 @@
+    
+    <section class="footer">
+
+        <div class="title">
+            <img src="images/main-logo.png" alt="logo">
+            <h1>Let's Taste it Once...</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui omnis dolores aliquid accusamus ipsum delectus iure maxime et eius corporis
+                ,corrupti ipsa. Dolores quisquam cumque porro eveniet debitis aspernatur modi consectetur quis beatae.
+            </p>
+            <div class="download">
+                <img src="images/store.png" alt="">
+            </div>
+            
+        </div>
+
+        <div class="menu">
+            <div class="head">
+                <h1>MENU</h1>
+            </div>
+            <a href="#">Home <i class="fas fa-arrow-right ms-3"></i></a>
+            <a href="#">Category <i class="fas fa-arrow-right ms-3"></i></a>
+            <a href="#">Foods <i class="fas fa-arrow-right ms-3"></i></a>
+            <a href="#">Contact <i class="fas fa-arrow-right ms-3"></i></a>
+            <a href="#">About <i class="fas fa-arrow-right ms-3"></i></a>
+        </div>
+
+        <div class="contact">
+            <div class="head">
+                <h1>CONTACT</h1>
+            </div>
+            <div class="add">
+                <i class="fas fa-map-marker me-3"></i>
+                Office - 19, Late Prin. V. K. Joag Path, Near Ruby Hall Clinic, Pune railway Station, Pune – 411001, Maharashtra, India
+            </div>
+            <div class="line"></div>
+            <div class="email"><i class="fas fa-envelope me-4"></i>hungry@yummy.com</div>
+            <div class="phone"><i class="fas fa-phone me-4"></i>+91 9588430618</div>
+            <div class="social">
+                <a href="https://www.facebook.com/profile.php?id=100092178187546"><img src="images/fb.png" alt=""></a>
+                <a href="https://x.com/AyushKesha85344"><img src="images/twtter.png" alt=""></a>
+                <a href="https://www.linkedin.com/in/ayushkesharwani1207/"><img src="images/in.png" alt=""></a>
+            </div>
+        </div>
+
+        <div class="line"></div>
+        <div class="last-footer">
+            <div class="cp">
+                <i class="fas fa-diamond me-3"></i>
+                CpTech Family
+            </div>
+            <div class="copy">
+                Copyright &#169; Yummy 2025 | All Rights are Reserved !
+            </div>
+        </div>
+
+    </section>
+    
+    
+    <!-- Swiper cdn for js -->
+    <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
+
+    <!-- JS link -->
+    <script src="js/script.js"></script>
+
+</body>
+</html>
